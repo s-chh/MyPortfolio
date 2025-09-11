7720311fdacc8fb016ff93a777dd3a4fa7eabd53
s-chh <s.chh@outlook.com>
2025-09-10 19:47:02 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,12 +12,4 @@ document.addEventListener('DOMContentLoaded', () => {
         loop: true
     });
 
-    // Mobile menu
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    });
 });
