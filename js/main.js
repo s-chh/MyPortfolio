1851eae28fad4e4e3c2f0ca689d429a690d01ae6
s-chh <s.chh@outlook.com>
2026-05-25 00:02:04 -0700

    Mobile ready

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Typing effect
-    new Typed('.typed', {
-        strings: [
-            'Machine Learning Scientist',
-            'PhD Graduate',
-            'Computer Vision Researcher',
-        ],
-        typeSpeed: 70,
-        backSpeed: 50,
-        backDelay: 2000,
-        loop: true
-    });
-
+    const typedEl = document.querySelector('.typed');
+    if (typedEl && typeof Typed !== 'undefined') {
+        new Typed('.typed', {
+            strings: [
+                'Machine Learning Scientist',
+                'PhD Graduate',
+                'Computer Vision Researcher',
+            ],
+            typeSpeed: 70,
+            backSpeed: 50,
+            backDelay: 2000,
+            loop: true,
+        });
+    }
 });
